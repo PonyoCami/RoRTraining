863b086d02d96e2986c1d114f72dfa339e91f831
Ponyo <gui.camelia@gmail.com>
2015-11-14 15:18:54 +0200

    cami first commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,41 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require_tree .
+//= require_self
+
+
+$ (document).ready(function()
+  {
+    if(window.location.href=="http://localhost:3000/")
+    {
+    $(location).attr('href', 'http://localhost:3000/ideas/')
+    }
+
+    if(window.location.href.indexOf("ideas")>0)
+    {
+      $("#ideas").addClass('active');
+    }
+
+    if(window.location.href.indexOf("info")>0)
+    {
+      $("#info").addClass('active');
+    }
+
+    if(window.location.href.indexOf("nicu")>0)
+    {
+      $("#nicu").addClass('active');
+    }
+  }
+)
